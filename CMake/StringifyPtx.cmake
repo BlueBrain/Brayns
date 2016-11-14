31b8c1baff1cb92aee87d9cf65f939132c275e6b
Cyrille Favreau <cyrille.favreau@epfl.ch>
2016-11-14 13:46:04 +0100

    Fixed rdumusc comments

--- CMake/StringifyPtx.cmake
+++ CMake/StringifyPtx.cmake
@@ -4,7 +4,7 @@
 #
 # This file is part of Brayns <https://github.com/BlueBrain/Brayns>
 
-set(CMAKE_STRINGIFY_PTX ${CMAKE_CURRENT_LIST_DIR})
+set(_cmake_stringify_ptx ${CMAKE_CURRENT_LIST_DIR}/StringifyPtx.cmake)
 
 function(stringify_ptx)
   set(_output_headers)
@@ -18,39 +18,39 @@ function(stringify_ptx)
     add_custom_command(OUTPUT ${_output_header} ${_output_source}
       COMMAND ${CMAKE_COMMAND} -DSTRINGIFY_PTX_PROCESSING_MODE=ON
         -DINPUT="${_input}" -DOUTPUT="${_output}"
-        -P ${CMAKE_STRINGIFY_PTX}/StringifyPtx.cmake
+        -P ${_cmake_stringify_ptx}
         DEPENDS ${_input}
       )
 
     list(APPEND _output_headers ${_output_header})
     list(APPEND _output_sources ${_output_source})
-  endforeach(_file ${ARGN})
+  endforeach()
 
   set(PTX_HEADERS ${_output_headers} PARENT_SCOPE)
   set(PTX_SOURCES ${_output_sources} PARENT_SCOPE)
 endfunction()
 
 if(STRINGIFY_PTX_PROCESSING_MODE)
-  get_filename_component(FILENAME ${INPUT} NAME)
-  string(REGEX REPLACE "[.]" "_" NAME ${FILENAME})
+  get_filename_component(_filename ${INPUT} NAME)
+  string(REGEX REPLACE "[.]" "_" _name ${_filename})
 
-  file(STRINGS ${INPUT} LINES)
+  file(STRINGS ${INPUT} _lines)
 
   file(WRITE ${OUTPUT}.h
     "/* Generated file, do not edit! */\n\n"
-    "extern char const* const ${NAME};\n"
-    )
+    "extern char const* const ${_name};\n"
+  )
 
   file(WRITE ${OUTPUT}.cpp
     "/* Generated file, do not edit! */\n\n"
-    "#include \"${FILENAME}.h\"\n\n"
-    "char const* const ${NAME} = \n"
-    )
+    "#include \"${_filename}.h\"\n\n"
+    "char const* const ${_name} = \n"
+  )
 
-  foreach(LINE ${LINES})
-    string(REPLACE "\"" "\\\"" LINE "${LINE}")
-    file(APPEND ${OUTPUT}.cpp "   \"${LINE}\\n\"\n")
-  endforeach(LINE)
+  foreach(_line ${_lines})
+    string(REPLACE "\"" "\\\"" _line "${_line}")
+    file(APPEND ${OUTPUT}.cpp "   \"${_line}\\n\"\n")
+  endforeach()
 
   file(APPEND ${OUTPUT}.cpp "   ;\n")
 endif()
