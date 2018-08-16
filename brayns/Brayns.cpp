BlueBrain/Brayns

commit d74faa4221e4f3eb9028f23483af615de6aecc24

Fix broadcast of animation params and scene updates
Animation previous/next frame updates from clients were not properly broadcasted.
Scene updates were too frequent and semantically wrong.

brayns/Brayns.cpp
@@ -180,12 +180,11 @@ struct Brayns::Impl : public PluginAPI
         {
 #ifdef BRAYNS_USE_LUNCHBOX
             if (isAsyncMode())
-            {
-                postSceneLoading();
                 return false;
-            }
 #endif
         }
+        else
+            _finishLoadScene();
 
         std::unique_lock<std::mutex> lock{_renderMutex, std::defer_lock};
         if (!lock.try_lock())
@@ -201,10 +200,6 @@ struct Brayns::Impl : public PluginAPI
 
         _engine->getStatistics().setSceneSizeInBytes(
             _engine->getScene().getSizeInBytes());
-
-        _sceneWasModified = _sceneWasModified || scene.isModified();
-        if (scene.isModified())
-            _finishLoadScene();
 
         _updateAnimation();
 
@@ -239,6 +234,10 @@ struct Brayns::Impl : public PluginAPI
             _engine->getFrameBuffer().clear();
         }
 
+        auto& ap = _parametersManager.getAnimationParameters();
+        _sceneWasModified = scene.isModified();
+        _animationParamsWereModified = ap.isModified();
+
         _parametersManager.resetModified();
         camera.resetModified();
         scene.resetModified();
@@ -258,26 +257,24 @@ struct Brayns::Impl : public PluginAPI
             _fpsUpdateElapsed = 0;
         }
 
-        // WAR to keep clients updated about current animation frame, or if the
-        // scene was modified, a simulation might have been attached. To fix
-        // this properly, we would need the simulation/animation state in model.
+        // broadcast changes on animations (playback) and scene (model
+        // add/remove)
         auto& ap = _parametersManager.getAnimationParameters();
-        if (ap.getDelta() != 0 || _sceneWasModified)
-        {
+        if (ap.getDelta() != 0 || _animationParamsWereModified)
             ap.markModified();
-            _sceneWasModified = false;
-        }
+
+        auto& scene = _engine->getScene();
+        if (_sceneWasModified)
+            scene.markModified();
 
         _extensionPluginFactory.postRender();
 
+        _sceneWasModified = false;
+        _animationParamsWereModified = false;
+
+        scene.resetModified();
         ap.resetModified();
         _engine->getFrameBuffer().resetModified();
-        _engine->getStatistics().resetModified();
-    }
-
-    void postSceneLoading()
-    {
-        _extensionPluginFactory.postSceneLoading();
         _engine->getStatistics().resetModified();
     }
 
@@ -474,9 +471,6 @@ private:
     {
         if (_dataLoadingFuture.valid())
             _dataLoadingFuture.get();
-
-        // finish reporting of progress
-        postSceneLoading();
     }
 
     void _updateAnimation()
@@ -1075,6 +1069,7 @@ private:
     ExtensionPluginFactory _extensionPluginFactory;
     std::shared_ptr<ActionInterface> _actionInterface;
     bool _sceneWasModified{false};
+    bool _animationParamsWereModified{false};
 };
 
 // -------------------------------------------------------------------------------------------------

brayns/common/scene/Scene.cpp
@@ -149,6 +149,9 @@ void Scene::removeModel(const size_t id)
     if (i == _modelDescriptors.end())
         return;
 
+    // we don't know which model used simulations, but we safely unset it.
+    setSimulationHandler(nullptr);
+
     _modelDescriptors.erase(i, _modelDescriptors.end());
     markModified();
 }

brayns/pluginapi/ExtensionPlugin.h
@@ -53,10 +53,6 @@ public:
     virtual void preRender() {}
     /** Called from Brayns::postRender() after render() has finished. */
     virtual void postRender() {}
-    /**
-     * Called after scene has finished loading.
-     */
-    virtual void postSceneLoading() {}
 };
 }
 

brayns/pluginapi/ExtensionPluginFactory.cpp
@@ -59,10 +59,4 @@ void ExtensionPluginFactory::postRender()
     for (ExtensionPluginPtr plugin : _plugins)
         plugin->postRender();
 }
-
-void ExtensionPluginFactory::postSceneLoading()
-{
-    for (ExtensionPluginPtr plugin : _plugins)
-        plugin->postSceneLoading();
-}
 }

brayns/pluginapi/ExtensionPluginFactory.h
@@ -59,12 +59,6 @@ public:
      */
     void postRender();
 
-    /**
-     * Calls postSceneLoading() on all registered plugins after scene has
-     * finished loading.
-     */
-    void postSceneLoading();
-
 private:
     ExtensionPlugins _plugins;
 };

plugins/RocketsPlugin/RocketsPlugin.cpp
@@ -233,17 +233,7 @@ public:
         _wsBroadcastOperations[ENDPOINT_ANIMATION_PARAMS]();
         _wsBroadcastOperations[METHOD_IMAGE_JPEG]();
         _wsBroadcastOperations[ENDPOINT_STATISTICS]();
-    }
-
-    void postSceneLoading()
-    {
-        if (!_rocketsServer)
-            return;
-
-        // simulation was potentially loaded
-        _wsBroadcastOperations[ENDPOINT_ANIMATION_PARAMS]();
-        _wsBroadcastOperations[ENDPOINT_SCENE]();      // bounds
-        _wsBroadcastOperations[ENDPOINT_STATISTICS](); // scene size
+        _wsBroadcastOperations[ENDPOINT_SCENE](); // bounds
     }
 
     void _setupRocketsServer()
@@ -1256,11 +1246,6 @@ void RocketsPlugin::preRender()
 void RocketsPlugin::postRender()
 {
     _impl->postRender();
-}
-
-void RocketsPlugin::postSceneLoading()
-{
-    _impl->postSceneLoading();
 }
 
 void RocketsPlugin::registerNotification(

plugins/RocketsPlugin/RocketsPlugin.h
@@ -54,12 +54,6 @@ public:
      */
     BRAYNS_API void postRender() final;
 
-    /**
-     * Enqueue modified and registered objects for broadcast that have changed
-     * after scene loading is finished (camera, progress, statistics).
-     */
-    BRAYNS_API void postSceneLoading() final;
-
 private:
     class Impl;
     std::shared_ptr<Impl> _impl;

commit 5de489749c8d7ebbaa25483d4dea4a2ae0f2faab

Fix missing scene updates on model bounding box and visibility
This was introduced by not handling a race condition introduced by d74faa4.

brayns/Brayns.cpp
@@ -257,23 +257,31 @@ struct Brayns::Impl : public PluginAPI
             _fpsUpdateElapsed = 0;
         }
 
-        // broadcast changes on animations (playback) and scene (model
-        // add/remove)
+        std::lock_guard<std::mutex> lock{_renderMutex};
+
         auto& ap = _parametersManager.getAnimationParameters();
         if (ap.getDelta() != 0 || _animationParamsWereModified)
+        {
+            _animationParamsWereModified = true;
             ap.markModified();
+        }
 
         auto& scene = _engine->getScene();
         if (_sceneWasModified)
             scene.markModified();
 
+        // broadcast changes on animations (playback) and scene (model
+        // add/remove)
         _extensionPluginFactory.postRender();
 
-        _sceneWasModified = false;
+        if (_animationParamsWereModified)
+            ap.resetModified();
         _animationParamsWereModified = false;
 
-        scene.resetModified();
-        ap.resetModified();
+        if (_sceneWasModified)
+            scene.resetModified();
+        _sceneWasModified = false;
+
         _engine->getFrameBuffer().resetModified();
         _engine->getStatistics().resetModified();
     }