f011c5322306cb90e32d2974efe84ccb3b80cf05
Jonas Karlsson <jonas.karlsson@epfl.ch>
2018-11-26 15:59:28 +0100

    Add documentation

--- brayns/common/utils/EnumUtils.h
+++ brayns/common/utils/EnumUtils.h
@@ -1,3 +1,23 @@
+/* Copyright (c) 2015-2017, EPFL/Blue Brain Project
+ * All rights reserved. Do not distribute without permission.
+ * Responsible Author: Jonas Karlsson <jonas.karlsson@epfl.ch>
+ *
+ * This file is part of Brayns <https://github.com/BlueBrain/Brayns>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #pragma once
 
 #include <string>
@@ -6,9 +26,17 @@
 
 namespace brayns
 {
+/*
+ * This file contains helper functions for converting strings to and from enums.
+ * To use the helper functions define the method 'enumMap' with a specialization
+ * for your enum type.
+ */
+
+/* Returns a mapping from a name to an enum type. */
 template <typename EnumT>
 std::vector<std::pair<std::string, EnumT>> enumMap();
 
+/* Returns all names for given enum type 'EnumT' */
 template <typename EnumT>
 inline std::vector<std::string> enumNames()
 {
@@ -18,6 +46,7 @@ inline std::vector<std::string> enumNames()
     return v;
 }
 
+/* Convert a string to an enum. */
 template <typename EnumT>
 inline EnumT stringToEnum(const std::string& v)
 {
@@ -29,6 +58,7 @@ inline EnumT stringToEnum(const std::string& v)
     return static_cast<EnumT>(0);
 }
 
+/* Convert an enum to a string. */
 template <typename EnumT>
 inline std::string enumToString(const EnumT v)
 {
