82817716671646463e8b4f209485ac899e929aae
Jonas Karlsson <jonaskarlsson@fripost.org>
2019-02-15 14:30:21 +0100

    Increase timer resolution to fix FPS numbers (#721)
    Previously using milliseconds which always gave framerate with several frames off

---
+++ brayns/common/Timer.cpp
@@ -0,0 +1,87 @@
+/* Copyright (c) 2018-2019, EPFL/Blue Brain Project
+ *
+ * This file is part of Brayns <https://github.com/BlueBrain/Brayns>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "Timer.h"
+
+#include <chrono>
+constexpr double MICRO_PER_SEC = 1000000.0;
+constexpr double FPS_UPDATE_MILLISECS = 150;
+
+namespace brayns
+{
+Timer::Timer()
+{
+    _lastFPSTickTime = clock::now();
+    _startTime = clock::now();
+}
+
+void Timer::start()
+{
+    _startTime = clock::now();
+}
+double Timer::elapsed() const
+{
+    return std::chrono::duration<double>{clock::now() - _startTime}.count();
+}
+
+void Timer::stop()
+{
+    const auto now = clock::now();
+    _microseconds =
+        std::chrono::duration_cast<std::chrono::microseconds>(now - _startTime)
+            .count();
+    _smoothNom = _smoothNom * _smoothingFactor + _microseconds / MICRO_PER_SEC;
+    _smoothDen = _smoothDen * _smoothingFactor + 1.f;
+
+    const auto secsLastFPSTick =
+        std::chrono::duration_cast<std::chrono::milliseconds>(now -
+                                                              _lastFPSTickTime)
+            .count();
+
+    if (secsLastFPSTick >= FPS_UPDATE_MILLISECS)
+    {
+        _lastFPSTickTime = now;
+        _fps = perSecond();
+    }
+}
+
+int64_t Timer::milliseconds() const
+{
+    return _microseconds / 1000.0;
+}
+
+double Timer::seconds() const
+{
+    return _microseconds / MICRO_PER_SEC;
+}
+
+double Timer::perSecond() const
+{
+    return MICRO_PER_SEC / _microseconds;
+}
+
+double Timer::perSecondSmoothed() const
+{
+    return _smoothDen / _smoothNom;
+}
+
+double Timer::fps() const
+{
+    return _fps;
+}
+}
--- brayns/common/Timer.h
+++ brayns/common/Timer.h
@@ -1,6 +1,4 @@
-/* Copyright (c) 2018, EPFL/Blue Brain Project
- *
- * Responsible Author: Daniel.Nachbaur@epfl.ch
+/* Copyright (c) 2018-2019, EPFL/Blue Brain Project
  *
  * This file is part of Brayns <https://github.com/BlueBrain/Brayns>
  *
@@ -24,50 +22,53 @@
 
 namespace brayns
 {
+using clock = std::chrono::high_resolution_clock;
+
 /** Simple timer class to measure time spent in a portion of the code */
 class Timer
 {
 public:
-    Timer() = default;
+    Timer();
 
     /** (Re)Start the timer at 'now' */
-    void start() { _startTime = clock::now(); }
-    /** @return the elapsed time in seconds since the last start(). */
-    double elapsed() const
-    {
-        return std::chrono::duration<double>{clock::now() - _startTime}.count();
-    }
+    void start();
 
     /** Stops the timer and records the interval + a smoothed value over time*/
-    void stop()
-    {
-        _milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(
-                            clock::now() - _startTime)
-                            .count();
-        _smoothNom = _smoothNom * _smoothingFactor + _milliseconds / 1000.0;
-        _smoothDen = _smoothDen * _smoothingFactor + 1.f;
-    }
+    void stop();
+
+    /** @return the elapsed time in seconds since the last start(). */
+    double elapsed() const;
 
     /** @return last interval from start() to stop() in milliseconds. */
-    int64_t milliseconds() const { return _milliseconds; }
+    int64_t milliseconds() const;
+
     /** @return last interval from start() to stop() in seconds. */
-    double seconds() const { return _milliseconds / 1000.0; }
+    double seconds() const;
+
     /**
      * @return last interval from start() to stop() in per seconds, e.g. for
      * frame per seconds
      */
-    double perSecond() const { return 1000.0 / _milliseconds; }
+    double perSecond() const;
+
+    /**
+     * @return the current FPS, updated every 150 ms
+     */
+    double fps() const;
+
     /**
      * @return last smoothed interval from start() to stop() in per seconds,
      * e.g. for frame per seconds
      */
-    double perSecondSmoothed() const { return _smoothDen / _smoothNom; }
+    double perSecondSmoothed() const;
+
 private:
-    using clock = std::chrono::high_resolution_clock;
     clock::time_point _startTime;
-    int64_t _milliseconds{0};
+    int64_t _microseconds{0};
     double _smoothNom{0.0};
     double _smoothDen{0.0};
     const double _smoothingFactor{0.9}; // closer to 1 means more smoothing
+    clock::time_point _lastFPSTickTime;
+    double _fps{0.0};
 };
 }
