a1e6e6c85064e44b2df3da68e1d380b5d7d485a4
Jonas Karlsson <jonas.karlsson@epfl.ch>
2019-02-27 12:04:24 +0100

    Simplify CUDA transfer function

--- engines/optix/cuda/renderer/BasicSimulation.cu
+++ engines/optix/cuda/renderer/BasicSimulation.cu
@@ -48,77 +48,42 @@ rtDeclareVariable(float2, value_range, , );
 rtBuffer<float> simulation_data;
 rtDeclareVariable(unsigned long, simulation_idx, attribute simulation_idx, );
 
-struct InterpolatedValue
+template <typename T>
+static __device__ inline T interpolateValues(const float v_min,
+                                             const float v_max,
+                                             const float value,
+                                             optix::buffer<T, 1> &values)
 {
-    float influence_low;
-    float influence_high;
-    int idx_low;
-    int idx_high;
-};
-
-// Finds between what pair of values a given value is and how much influence
-// each of these values have
-static __device__ inline InterpolatedValue interpolateValues(
-    const float v_min, const float v_max, const float value,
-    const int num_values)
-{
-    InterpolatedValue result;
+    const int num_values = values.size() / sizeof(T);
 
     const float v_clamped = min(v_max, max(v_min, value));
     const float range_per_value = (v_max - v_min) / (num_values - 1);
     const float idx_value = (v_clamped - v_min) / range_per_value;
 
-    // Indices of the two values to interpolate between
-    const int idx_value_low = max(0, int(floor(idx_value)));
-    const int idx_value_high = min(num_values - 1, int(ceil(idx_value)));
+    const int index = int(floor(idx_value));
 
-    result.idx_low = idx_value_low;
-    result.idx_high = idx_value_high;
+    if (index == num_values - 1)
+        return values[index];
 
-    if (idx_value_low == idx_value_high)
-    {
-        // Since both values are the same we split the influence
-        result.influence_low = 0.5f;
-        result.influence_high = 0.5f;
-    }
-    else
-    {
-        const float v_low = v_min + float(idx_value_low) * range_per_value;
-        const float v_high = v_min + float(idx_value_high) * range_per_value;
+    const float v_low = v_min + float(index) * range_per_value;
+    const float t = (v_clamped - v_low) / range_per_value;
 
-        result.influence_low = 1.0f - abs(v_clamped - v_low) / range_per_value;
-        result.influence_high =
-            1.0f - abs(v_clamped - v_high) / range_per_value;
-    }
-
-    return result;
+    return values[index] * (1.0f - t) + values[index + 1] * t;
 }
 
 static __device__ inline float3 calcTransferFunctionColor()
 {
     const float3 WHITE = make_float3(1.f, 1.f, 1.f);
-    float3 color_opaque;
-    float opacity;
 
     const float x_min = value_range.x;
     const float x_max = value_range.y;
     const float x_value = simulation_data[simulation_idx];
 
-    {
-        const int num_colors = colors.size() / (sizeof(float3));
-        const InterpolatedValue iv =
-            interpolateValues(x_min, x_max, x_value, num_colors);
-        color_opaque = colors[iv.idx_low] * iv.influence_low +
-                       colors[iv.idx_high] * iv.influence_high;
-    }
+    const float3 color_opaque =
+        interpolateValues<float3>(x_min, x_max, x_value, colors);
 
-    {
-        const int num_opacities = opacities.size() / (sizeof(float));
-        const InterpolatedValue iv =
-            interpolateValues(x_min, x_max, x_value, num_opacities);
-        opacity = opacities[iv.idx_low] * iv.influence_low +
-                  opacities[iv.idx_high] * iv.influence_high;
-    }
+    const float opacity =
+        interpolateValues<float>(x_min, x_max, x_value, opacities);
 
     return opacity * color_opaque + (1.0f - opacity) * WHITE;
 }
