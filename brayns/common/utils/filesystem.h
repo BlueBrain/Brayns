87d6ce72b1d956892b77b055a4069d48fa82bd6c
Jonas Karlsson <jonas.karlsson@epfl.ch>
2019-08-14 16:15:03 +0200

    Use custom filesystem implementation

--- brayns/common/utils/filesystem.h
+++ brayns/common/utils/filesystem.h
@@ -17,43 +17,6 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-// https://stackoverflow.com/a/53365539
-
-// clang-format off
-
-#ifndef INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
-#   if defined(__cpp_lib_filesystem)
-#       define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
-#   elif defined(__cpp_lib_experimental_filesystem)
-#       define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-#   elif !defined(__has_include)
-#       define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-#   elif __has_include(<filesystem>)
-#       ifdef _MSC_VER
-#           if __has_include(<yvals_core.h>)
-#               include <yvals_core.h>
-#               if defined(_HAS_CXX17) && _HAS_CXX17
-#                   define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
-#               endif
-#           endif
-#           ifndef INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
-#               define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-#           endif
-#       else // #ifdef _MSC_VER
-#           define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 0
-#       endif
-#   elif __has_include(<experimental/filesystem>)
-#       define INCLUDE_STD_FILESYSTEM_EXPERIMENTAL 1
-#   else
-#       error Could not find system header "<filesystem>" or "<experimental/filesystem>"
-#   endif
-#   if INCLUDE_STD_FILESYSTEM_EXPERIMENTAL
-#       include <experimental/filesystem>
-        namespace fs = std::experimental::filesystem;
-#   else
-#       include <filesystem>
-        namespace fs = std::filesystem;
-#   endif
-#endif
-
-// clang-format on
+// NOTE: replace with std::filesystem when upgrading to c++17
+#include <deps/filesystem.hpp>
+namespace fs = ghc::filesystem;
