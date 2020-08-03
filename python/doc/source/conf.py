da6ed2eaee44a0f50ad50e3a9018f24848ce8b90
Nadir Roman Guerrero <nadir.ro.gue@gmail.com>
2020-08-03 11:51:48 +0200

    Fix read the docs build

--- python/doc/source/conf.py
+++ python/doc/source/conf.py
@@ -11,8 +11,15 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
+import os
 import sys
-from brayns.version import VERSION
+sys.path.insert(0, os.path.abspath('../../brayns'))
+
+from version import VERSION
+
+from os import path
+sys.path.insert(0, path.dirname(path.dirname(path.abspath('../python'))))
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
