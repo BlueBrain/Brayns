3577fb32132f99ed17484b5beaadaae59a62f4e0
Adrien4193 <adrien.fleury93@gmail.com>
2021-09-17 14:41:13 +0200

    Network task documentation.

--- brayns/network/tasks/NetworkTask.h
+++ brayns/network/tasks/NetworkTask.h
@@ -28,9 +28,22 @@
 
 namespace brayns
 {
+/**
+ * @brief Synchronized wrapper around an arbitrary exception.
+ *
+ * Can be used to set an exception to a task from the task thread or the main
+ * thread.
+ *
+ */
 class NetworkTaskException
 {
 public:
+    /**
+     * @brief Rethrow the contained exception.
+     *
+     * Throw std::runtime_error with generic cancellation message if not set.
+     *
+     */
     void rethrow()
     {
         std::lock_guard<std::mutex> lock(_mutex);
@@ -41,6 +54,12 @@ public:
         std::rethrow_exception(_e);
     }
 
+    /**
+     * @brief Assign an arbitrary exception.
+     *
+     * @param e Opaque exception pointer.
+     * @return NetworkTaskException& *this.
+     */
     NetworkTaskException& operator=(std::exception_ptr e)
     {
         std::lock_guard<std::mutex> lock(_mutex);
@@ -48,6 +67,13 @@ public:
         return *this;
     }
 
+    /**
+     * @brief Assign an arbitrary exception.
+     *
+     * @tparam T Exception type.
+     * @param e Exception instance.
+     * @return NetworkTaskException& *this
+     */
     template <typename T>
     NetworkTaskException& operator=(T e)
     {
@@ -60,19 +86,61 @@ private:
     std::exception_ptr _e;
 };
 
+/**
+ * @brief Base class to run a network request in a parallel thread.
+ *
+ * Provides cancellation and polling support to monitor the task from the main
+ * thread.
+ *
+ * The task functionality must be implemented by overridding the run() method.
+ *
+ * Only the content of the run() method will be executed in a separated thread.
+ *
+ */
 class NetworkTask
 {
 public:
     virtual ~NetworkTask() = default;
 
+    /**
+     * @brief Check wether the task result is ready to be retreived.
+     *
+     * @return true Task is done and a the result has not been retreived yet.
+     * @return false Task is running or the result has already been retreived.
+     */
     bool isReady() const { return _hasStatus(std::future_status::ready); }
 
+    /**
+     * @brief Check if the task is running.
+     *
+     * @return true Task is running.
+     * @return false Task is done or not started.
+     */
     bool isRunning() const { return _hasStatus(std::future_status::timeout); }
 
+    /**
+     * @brief Check if the task has been cancelled.
+     *
+     * @return true Cancel was called between start() and onComplete().
+     * @return false Task was not cancelled or has been restarted.
+     */
     bool isCancelled() const { return _cancelled; }
 
+    /**
+     * @brief Check if the task can be cancelled.
+     *
+     * Cancelling a task is meaningful if the task is running and not already
+     * cancelled.
+     *
+     * @return true Task cancellation is meaningful.
+     * @return false Task cannot be cancelled.
+     */
     bool isCancellable() const { return isRunning() && !isCancelled(); }
 
+    /**
+     * @brief Start the task by calling run() in a separated thread.
+     *
+     */
     void start()
     {
         cancelAndWait();
@@ -81,6 +149,10 @@ public:
         _result = std::async(std::launch::async, [this] { run(); });
     }
 
+    /**
+     * @brief Retreive task result if this one is ready to be fetched.
+     *
+     */
     void poll()
     {
         if (!isReady())
@@ -90,6 +162,10 @@ public:
         _fetchResult();
     }
 
+    /**
+     * @brief Retreive task result or wait for it if not ready.
+     *
+     */
     void wait()
     {
         if (!_result.valid())
@@ -99,6 +175,10 @@ public:
         _fetchResult();
     }
 
+    /**
+     * @brief Cancel the task if cancellable.
+     *
+     */
     void cancel()
     {
         if (!isCancellable())
@@ -109,20 +189,41 @@ public:
         _cancelled = true;
     }
 
+    /**
+     * @brief Assign an exception to the task (thread safe).
+     *
+     * The exception will be passed to the onError(exception) method on the next
+     * poll() from the main thread.
+     *
+     * @param e Opaque exception pointer.
+     */
     void setException(std::exception_ptr e) { _e = e; }
 
+    /**
+     * @brief Shortcut to assign an exception with cancellation.
+     *
+     * @param e  Opaque exception pointer.
+     */
     void cancelWith(std::exception_ptr e)
     {
         _e = e;
         cancel();
     }
 
+    /**
+     * @brief Cancel the task and wait for its termination.
+     *
+     */
     void cancelAndWait()
     {
         cancel();
         wait();
     }
 
+    /**
+     * @brief Throw the current exception set if the task is cancelled.
+     *
+     */
     void checkCancelled()
     {
         if (!_cancelled)
@@ -132,32 +233,80 @@ public:
         _e.rethrow();
     }
 
+    /**
+     * @brief Check cancellation and call onProgress() implementation.
+     *
+     * @param operation Current operation description.
+     * @param amount Percentage progress 0-1.
+     */
     void progress(const std::string& operation, double amount)
     {
         checkCancelled();
         onProgress(operation, amount);
     }
 
+    /**
+     * @brief Implementation of the task that will run in a separated thread.
+     *
+     */
     virtual void run() = 0;
 
+    /**
+     * @brief Custom logic to run in the main thread when a task is started.
+     *
+     */
     virtual void onStart() {}
 
+    /**
+     * @brief Custom logic to run in the main thread when a task succeed.
+     *
+     */
     virtual void onComplete() {}
 
+    /**
+     * @brief Custom logic to run in the main thread just before cancellation.
+     *
+     */
     virtual void onCancel() {}
 
+    /**
+     * @brief Custom logic to run in the main thread when an exception is set.
+     *
+     */
     virtual void onError(std::exception_ptr) {}
 
+    /**
+     * @brief Custom logic to run when progress() is called.
+     *
+     * @param operation Current operation description.
+     * @param amount Percentage progress 0-1.
+     */
     virtual void onProgress(const std::string&, double) {}
 
+    /**
+     * @brief Called when the task client is disconnected.
+     *
+     */
     virtual void onDisconnect() {}
 
+    /**
+     * @brief Assign an exception to the task.
+     *
+     * @tparam T Exception type.
+     * @param e Exception instance.
+     */
     template <typename T>
     void setException(T e)
     {
         _e = std::move(e);
     }
 
+    /**
+     * @brief Cancel the task with the given exception.
+     *
+     * @tparam T Exception type.
+     * @param e Exception instance.
+     */
     template <typename T>
     void cancelWith(T e)
     {
