3ffaa5fc4f73f02a621323d88b1b3222b03c5f48
Adrien4193 <adrien.fleury93@gmail.com>
2022-01-31 10:57:24 +0100

    network/tasks moved.

--- brayns/network/tasks/NetworkTask.h
+++ brayns/network/tasks/NetworkTask.h
@@ -22,7 +22,6 @@
 #pragma once
 
 #include <atomic>
-#include <chrono>
 #include <future>
 #include <memory>
 #include <mutex>
@@ -46,15 +45,7 @@ public:
      * Throw std::runtime_error with generic cancellation message if not set.
      *
      */
-    void rethrow()
-    {
-        std::lock_guard<std::mutex> lock(_mutex);
-        if (!_e)
-        {
-            _e = std::make_exception_ptr(std::runtime_error("Task cancelled"));
-        }
-        std::rethrow_exception(_e);
-    }
+    void rethrow();
 
     /**
      * @brief Assign an arbitrary exception.
@@ -62,12 +53,7 @@ public:
      * @param e Opaque exception pointer.
      * @return NetworkTaskException& *this.
      */
-    NetworkTaskException &operator=(std::exception_ptr e)
-    {
-        std::lock_guard<std::mutex> lock(_mutex);
-        _e = std::move(e);
-        return *this;
-    }
+    NetworkTaskException &operator=(std::exception_ptr e);
 
     /**
      * @brief Assign an arbitrary exception.
@@ -110,10 +96,7 @@ public:
      * @return true Task is done and a the result has not been retreived yet.
      * @return false Task is running or the result has already been retreived.
      */
-    bool isReady() const
-    {
-        return _hasStatus(std::future_status::ready);
-    }
+    bool isReady() const;
 
     /**
      * @brief Check if the task is running.
@@ -121,10 +104,7 @@ public:
      * @return true Task is running.
      * @return false Task is done or not started.
      */
-    bool isRunning() const
-    {
-        return _hasStatus(std::future_status::timeout);
-    }
+    bool isRunning() const;
 
     /**
      * @brief Check if the task has been cancelled.
@@ -132,10 +112,7 @@ public:
      * @return true Cancel was called between start() and onComplete().
      * @return false Task was not cancelled or has been restarted.
      */
-    bool isCancelled() const
-    {
-        return _cancelled;
-    }
+    bool isCancelled() const;
 
     /**
      * @brief Check if the task can be cancelled.
@@ -146,62 +123,31 @@ public:
      * @return true Task cancellation is meaningful.
      * @return false Task cannot be cancelled.
      */
-    bool isCancellable() const
-    {
-        return isRunning() && !isCancelled();
-    }
+    bool isCancellable() const;
 
     /**
      * @brief Start the task by calling run() in a separated thread.
      *
      */
-    void start()
-    {
-        cancelAndWait();
-        onStart();
-        _cancelled = false;
-        _result = std::async(std::launch::async, [this] { run(); });
-    }
+    void start();
 
     /**
      * @brief Retreive task result if this one is ready to be fetched.
      *
      */
-    void poll()
-    {
-        if (!isReady())
-        {
-            return;
-        }
-        _fetchResult();
-    }
+    void poll();
 
     /**
      * @brief Retreive task result or wait for it if not ready.
      *
      */
-    void wait()
-    {
-        if (!_result.valid())
-        {
-            return;
-        }
-        _fetchResult();
-    }
+    void wait();
 
     /**
      * @brief Cancel the task if cancellable.
      *
      */
-    void cancel()
-    {
-        if (!isCancellable())
-        {
-            return;
-        }
-        onCancel();
-        _cancelled = true;
-    }
+    void cancel();
 
     /**
      * @brief Assign an exception to the task (thread safe).
@@ -211,44 +157,26 @@ public:
      *
      * @param e Opaque exception pointer.
      */
-    void setException(std::exception_ptr e)
-    {
-        _e = e;
-    }
+    void setException(std::exception_ptr e);
 
     /**
      * @brief Shortcut to assign an exception with cancellation.
      *
      * @param e  Opaque exception pointer.
      */
-    void cancelWith(std::exception_ptr e)
-    {
-        _e = e;
-        cancel();
-    }
+    void cancelWith(std::exception_ptr e);
 
     /**
      * @brief Cancel the task and wait for its termination.
      *
      */
-    void cancelAndWait()
-    {
-        cancel();
-        wait();
-    }
+    void cancelAndWait();
 
     /**
      * @brief Throw the current exception set if the task is cancelled.
      *
      */
-    void checkCancelled()
-    {
-        if (!_cancelled)
-        {
-            return;
-        }
-        _e.rethrow();
-    }
+    void checkCancelled();
 
     /**
      * @brief Check cancellation and call onProgress() implementation.
@@ -256,11 +184,7 @@ public:
      * @param operation Current operation description.
      * @param amount Percentage progress 0-1.
      */
-    void progress(const std::string &operation, double amount)
-    {
-        checkCancelled();
-        onProgress(operation, amount);
-    }
+    void progress(const std::string &operation, double amount);
 
     /**
      * @brief Implementation of the task that will run in a separated thread.
@@ -272,33 +196,25 @@ public:
      * @brief Custom logic to run in the main thread when a task is started.
      *
      */
-    virtual void onStart()
-    {
-    }
+    virtual void onStart();
 
     /**
      * @brief Custom logic to run in the main thread when a task succeed.
      *
      */
-    virtual void onComplete()
-    {
-    }
+    virtual void onComplete();
 
     /**
      * @brief Custom logic to run in the main thread just before cancellation.
      *
      */
-    virtual void onCancel()
-    {
-    }
+    virtual void onCancel();
 
     /**
      * @brief Custom logic to run in the main thread when an exception is set.
      *
      */
-    virtual void onError(std::exception_ptr)
-    {
-    }
+    virtual void onError(std::exception_ptr);
 
     /**
      * @brief Custom logic to run when progress() is called.
@@ -306,17 +222,13 @@ public:
      * @param operation Current operation description.
      * @param amount Percentage progress 0-1.
      */
-    virtual void onProgress(const std::string &, double)
-    {
-    }
+    virtual void onProgress(const std::string &, double);
 
     /**
      * @brief Called when the task client is disconnected.
      *
      */
-    virtual void onDisconnect()
-    {
-    }
+    virtual void onDisconnect();
 
     /**
      * @brief Assign an exception to the task.
@@ -344,28 +256,8 @@ public:
     }
 
 private:
-    bool _hasStatus(std::future_status status) const
-    {
-        if (!_result.valid())
-        {
-            return false;
-        }
-        return _result.wait_for(std::chrono::seconds(0)) == status;
-    }
-
-    void _fetchResult()
-    {
-        try
-        {
-            _result.get();
-            checkCancelled();
-            onComplete();
-        }
-        catch (...)
-        {
-            onError(std::current_exception());
-        }
-    }
+    bool _hasStatus(std::future_status status) const;
+    void _fetchResult();
 
     std::future<void> _result;
     std::atomic_bool _cancelled{false};
--- brayns/network/tasks/NetworkTaskManager.h
+++ brayns/network/tasks/NetworkTaskManager.h
@@ -21,10 +21,145 @@
 
 #pragma once
 
-#include "NetworkTaskMap.h"
+#include <string>
+#include <unordered_map>
+
+#include <brayns/network/json/RequestId.h>
+#include <brayns/network/socket/ConnectionHandle.h>
+
+#include "NetworkTask.h"
 
 namespace brayns
 {
+/**
+ * @brief Task pool indexed by request ID.
+ *
+ */
+using NetworkTaskIdMap = std::unordered_map<RequestId, NetworkTaskPtr>;
+
+/**
+ * @brief Map of task pools indexed by client connection handle.
+ *
+ */
+using NetworkTaskConnectionMap = std::unordered_map<ConnectionHandle, NetworkTaskIdMap>;
+
+/**
+ * @brief Pool to store tasks.
+ *
+ * Allow to store and retreive tasks from the connection handle of the client
+ * requesting it and the ID of the associated request.
+ *
+ */
+class NetworkTaskMap
+{
+public:
+    /**
+     * @brief Add a task to the pool with the associated client and request ID.
+     *
+     * @param handle Connection handle of the client requesting the task.
+     * @param id ID of the request starting the task.
+     * @param task Abstract task (might be running or not).
+     */
+    void add(const ConnectionHandle &handle, const RequestId &id, NetworkTaskPtr task);
+
+    /**
+     * @brief Retreive a task using its client and request ID.
+     *
+     * @param handle Connection handle of the client requesting the task.
+     * @param id ID of the request starting the task.
+     * @return NetworkTask* Task or null if not found.
+     */
+    NetworkTask *find(const ConnectionHandle &handle, const RequestId &id) const;
+
+    /**
+     * @brief Call the given functor on each client, request ID, task.
+     *
+     * Functor: void(const ConnectionHandle&, const RequestId&, NetworkTask&).
+     *
+     * @tparam FunctorType Functor type.
+     * @param functor Functor instance.
+     */
+    template<typename FunctorType>
+    void forEach(FunctorType functor) const
+    {
+        for (const auto &pair : _tasks)
+        {
+            auto &handle = pair.first;
+            auto &tasks = pair.second;
+            for (const auto &child : tasks)
+            {
+                auto &id = child.first;
+                auto &task = child.second;
+                functor(handle, id, *task);
+            }
+        }
+    }
+
+    /**
+     * @brief Call for each request ID, task required by the given client.
+     *
+     * Functor: void(const RequestId&, NetworkTask&).
+     *
+     * @tparam FunctorType Functor type.
+     * @param handle Client connection handle.
+     * @param functor Functor instance.
+     */
+    template<typename FunctorType>
+    void forEach(const ConnectionHandle &handle, FunctorType functor) const
+    {
+        auto i = _tasks.find(handle);
+        if (i == _tasks.end())
+        {
+            return;
+        }
+        auto &tasks = i->second;
+        for (const auto &pair : tasks)
+        {
+            auto &id = pair.first;
+            auto &task = pair.second;
+            functor(id, *task);
+        }
+    }
+
+    /**
+     * @brief Remove tasks if the given functor returns true.
+     *
+     * Functor: bool(const ConnectionHandle&, const RequestId&, NetworkTask&).
+     *
+     * @tparam FunctorType Functor type.
+     * @param functor Functor instance.
+     */
+    template<typename FunctorType>
+    void removeIf(FunctorType functor)
+    {
+        for (auto i = _tasks.begin(); i != _tasks.end();)
+        {
+            auto &handle = i->first;
+            auto &tasks = i->second;
+            for (auto j = tasks.begin(); j != tasks.end();)
+            {
+                auto &id = j->first;
+                auto &task = j->second;
+                if (functor(handle, id, *task))
+                {
+                    j = tasks.erase(j);
+                    continue;
+                }
+                ++j;
+            }
+            if (tasks.empty())
+            {
+                i = _tasks.erase(i);
+                continue;
+            }
+            ++i;
+        }
+    }
+
+private:
+    NetworkTaskConnectionMap _tasks;
+};
+
 /**
  * @brief Task manager to store all running tasks started by clients.
  *
@@ -42,15 +177,7 @@ public:
      * @param id Task request ID from JSON-RPC.
      * @param task Task to register.
      */
-    void addOrReplace(const ConnectionHandle &handle, const RequestId &id, NetworkTaskPtr task)
-    {
-        auto oldTask = _tasks.find(handle, id);
-        if (oldTask)
-        {
-            oldTask->cancelAndWait();
-        }
-        _tasks.add(handle, id, std::move(task));
-    }
+    void addOrReplace(const ConnectionHandle &handle, const RequestId &id, NetworkTaskPtr task);
 
     /**
      * @brief Add a task if not already running.
@@ -61,26 +188,14 @@ public:
      * @return true No task with the same client / ID exists.
      * @return false Cannot register the task because already running.
      */
-    bool addIfNotPresent(const ConnectionHandle &handle, const RequestId &id, NetworkTaskPtr task)
-    {
-        auto oldTask = _tasks.find(handle, id);
-        if (oldTask)
-        {
-            return false;
-        }
-        _tasks.add(handle, id, std::move(task));
-        return true;
-    }
+    bool addIfNotPresent(const ConnectionHandle &handle, const RequestId &id, NetworkTaskPtr task);
 
     /**
      * @brief Notify all task bound to a client when this one disconnects.
      *
      * @param handle Client handle.
      */
-    void disconnect(const ConnectionHandle &handle) const
-    {
-        _tasks.forEach(handle, [&](auto &, auto &task) { task.onDisconnect(); });
-    }
+    void disconnect(const ConnectionHandle &handle) const;
 
     /**
      * @brief Cancel a task from its client and ID.
@@ -90,16 +205,7 @@ public:
      * @return true Task found and cancelled.
      * @return false Task not found.
      */
-    bool cancel(const ConnectionHandle &handle, const RequestId &id) const
-    {
-        auto task = _tasks.find(handle, id);
-        if (!task)
-        {
-            return false;
-        }
-        task->cancel();
-        return true;
-    }
+    bool cancel(const ConnectionHandle &handle, const RequestId &id) const;
 
     /**
      * @brief Poll all registered task.
@@ -108,16 +214,7 @@ public:
      * from the list.
      *
      */
-    void poll()
-    {
-        _tasks.removeIf(
-            [](auto &, auto &, auto &task)
-            {
-                bool complete = !task.isRunning();
-                task.poll();
-                return complete;
-            });
-    }
+    void poll();
 
 private:
     NetworkTaskMap _tasks;
