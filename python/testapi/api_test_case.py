5394410b300619b06531ee9d35d9af9c4bfcf8e4
Adrien4193 <adrien.fleury93@gmail.com>
2022-06-27 10:09:15 +0200

    Test API.

---
+++ python/testapi/api_test_case.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2015-2022 EPFL/Blue Brain Project
+# All rights reserved. Do not distribute without permission.
+#
+# Responsible Author: adrien.fleury@epfl.ch
+#
+# This file is part of Brayns <https://github.com/BlueBrain/Brayns>
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3.0 as published
+# by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import os
+import unittest
+
+import brayns
+
+
+class ApiTestCase(unittest.TestCase):
+
+    @property
+    def executable(self) -> str:
+        return os.environ['BRAYNS_TEST_EXECUTABLE']
+
+    @property
+    def env(self) -> dict[str, str]:
+        result = dict[str, str]()
+        ospray = os.environ.get('BRAYNS_TEST_OSPRAY_DIR')
+        if ospray is not None:
+            result['LD_LIBRARY_PATH'] = ospray
+        return result
+
+    def create_launcher(self) -> brayns.Launcher:
+        return brayns.Launcher(
+            executable=self.executable,
+            env=self.env
+        )
