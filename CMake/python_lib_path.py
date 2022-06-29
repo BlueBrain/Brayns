4a6a31dab10bd161c46e1bd2e8e57b97976518d5
Nadir Roman Guerrero <nadir.ro.gue@gmail.com>
2022-06-29 16:17:00 +0200

    Addressing reviews

--- CMake/python_lib_path.py
+++ CMake/python_lib_path.py
@@ -1,6 +1,7 @@
 from sysconfig import get_path, sys
+from pathlib import Path
 
-full_path = get_path('platlib')
-prefix = sys.prefix + '/'
-relative_path = full_path.replace(prefix, '')
+full_path = Path(get_path('platlib'))
+prefix = Path(sys.prefix)
+relative_path = full_path.relative_to(prefix)
 print(relative_path)
