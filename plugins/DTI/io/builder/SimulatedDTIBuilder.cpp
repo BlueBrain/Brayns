35dca42485d0fb0e772618aecd6953682a9b33eb
Nadir Román Guerrero <NadirRoGue@users.noreply.github.com>
2022-07-05 09:10:11 +0200

    BRAYNS 411 - Switch from .ini to .json as DTI file format (#1044)

---
+++ plugins/DTI/io/builder/SimulatedDTIBuilder.cpp
@@ -0,0 +1,109 @@
+/* Copyright (c) 2015-2022, EPFL/Blue Brain Project
+ * All rights reserved. Do not distribute without permission.
+ * Responsible Author: Nadir Roman Guerrero <nadir.romanguerrero@epfl.ch>
+ *
+ * This file is part of Brayns <https://github.com/BlueBrain/Brayns>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "SimulatedDTIBuilder.h"
+
+#include "common/RowTreamlineMapReader.h"
+#include "common/StreamlineComponentBuilder.h"
+
+#include <components/SpikeReportComponent.h>
+
+#include <brayns/utils/FileReader.h>
+
+#include <brain/spikeReportReader.h>
+#include <brion/blueConfig.h>
+
+#include <unordered_set>
+
+namespace
+{
+class GIDsToStreamlineIndicesMapping
+{
+public:
+    static std::unordered_map<uint64_t, std::vector<size_t>> generate(
+        const std::vector<dti::SimulatedDTIBuilder::GIDRow> &gidRows,
+        const std::map<uint64_t, dti::StreamlineData> &streamlinesMap)
+    {
+        std::unordered_map<uint64_t, std::vector<size_t>> result;
+        for (const auto &gidRow : gidRows)
+        {
+            const auto gid = gidRow.gid;
+            const auto row = gidRow.row;
+
+            const auto &streamline = streamlinesMap.at(row);
+            const auto streamlineIndex = streamline.linealIndex;
+
+            auto &gidIndexList = result[gid];
+            gidIndexList.push_back(streamlineIndex);
+        }
+
+        return result;
+    }
+};
+}
+
+namespace dti
+{
+std::istream &operator>>(std::istream &in, SimulatedDTIBuilder::GIDRow &gr)
+{
+    return in >> gr.gid >> gr.row;
+}
+
+void SimulatedDTIBuilder::reset()
+{
+    _gidRows.clear();
+    _streamlines.clear();
+}
+
+void SimulatedDTIBuilder::readGidRowFile(const std::string &path)
+{
+    const auto content = brayns::FileReader::read(path);
+    std::istringstream stream(content);
+    _gidRows = std::vector<GIDRow>(std::istream_iterator<GIDRow>(stream), {});
+}
+
+void SimulatedDTIBuilder::readStreamlinesFile(const std::string &path)
+{
+    std::unordered_set<size_t> whitelistedRows;
+    for (const auto &entry : _gidRows)
+    {
+        const auto row = entry.row;
+        whitelistedRows.insert(row);
+    }
+
+    _streamlines = RowStreamlineMapReader::read(
+        path,
+        [&](size_t row) { return whitelistedRows.find(row) != whitelistedRows.end(); });
+}
+
+void SimulatedDTIBuilder::buildGeometry(float radius, brayns::Model &model)
+{
+    StreamlineComponentBuilder::build(_streamlines, radius, model);
+}
+
+void SimulatedDTIBuilder::buildSimulation(const std::string &path, float spikeDecayTime, brayns::Model &model)
+{
+    auto gidStreamlineMap = GIDsToStreamlineIndicesMapping::generate(_gidRows, _streamlines);
+    const auto blueConfig = brion::BlueConfig(path);
+    const auto spikesURI = blueConfig.getSpikeSource();
+    auto spikeReport = std::make_unique<brain::SpikeReportReader>(spikesURI);
+    model.addComponent<dti::SpikeReportComponent>(std::move(spikeReport), std::move(gidStreamlineMap), spikeDecayTime);
+}
+}
---
+++ plugins/DTI/io/builder/common/StreamlineComponentBuilder.cpp
@@ -0,0 +1,53 @@
+/* Copyright (c) 2015-2022, EPFL/Blue Brain Project
+ * All rights reserved. Do not distribute without permission.
+ * Responsible Author: Nadir Roman Guerrero <nadir.romanguerrero@epfl.ch>
+ *
+ * This file is part of Brayns <https://github.com/BlueBrain/Brayns>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "StreamlineComponentBuilder.h"
+
+#include <brayns/engine/geometry/types/Primitive.h>
+
+#include <components/DTIComponent.h>
+
+namespace dti
+{
+void StreamlineComponentBuilder::build(
+    const std::map<uint64_t, StreamlineData> &streamlines,
+    float radius,
+    brayns::Model &model)
+{
+    std::vector<std::vector<brayns::Primitive>> geometries;
+    geometries.reserve(streamlines.size());
+
+    for (const auto &[row, streamline] : streamlines)
+    {
+        const auto &points = streamline.points;
+
+        auto &geometry = geometries.emplace_back();
+        geometry.reserve(points.size() - 1);
+
+        for (size_t i = 1; i < points.size(); ++i)
+        {
+            const auto &start = points[i - 1];
+            const auto &end = points[i];
+            geometry.push_back(brayns::Primitive::cylinder(start, end, radius));
+        }
+    }
+    model.addComponent<dti::DTIComponent>(std::move(geometries));
+}
+}
