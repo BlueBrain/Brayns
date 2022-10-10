74dc4a5f19fa608400c2aa39cd0a8eb90fdd6f41
Adrien4193 <adrien.fleury93@gmail.com>
2022-10-10 16:23:00 +0200

    Circuit thickness testapi.

---
+++ python/testapi/plugins/circuit/test_set_circuit_thickness.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2015-2022 EPFL/Blue Brain Project
+# All rights reserved. Do not distribute without permission.
+#
+# Responsible Author: adrien.fleury@epfl.ch
+#
+# This file is part of Brayns <https://github.com/BlueBrain/Brayns>
+#
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License version 3.0 as published
+# by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import brayns
+from testapi.simple_test_case import SimpleTestCase
+
+
+class TestSetCircuitThickness(SimpleTestCase):
+
+    def test_set_circuit_thickness(self) -> None:
+        id = self._load_model()
+        brayns.set_circuit_thickness(self.instance, id, 3)
+
+    def _load_model(self) -> int:
+        loader = brayns.BbpLoader()
+        models = loader.load_models(self.instance, self.bbp_circuit)
+        return models[0].id
