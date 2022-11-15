62c6165c9e51a9720685c2c90b0fbc20984594d7
Adrien4193 <adrien.fleury93@gmail.com>
2022-11-15 08:35:00 +0100

    CI test.

--- python/brayns/service/start.py
+++ python/brayns/service/start.py
@@ -18,6 +18,8 @@
 # along with this library; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+import time
+
 from brayns.network import Connector
 
 from .manager import Manager
@@ -37,6 +39,8 @@ def start(service: Service, connector: Connector) -> Manager:
     :rtype: Manager
     """
     process = service.start()
+    time.sleep(10)
+    print(process.logs)
     try:
         instance = connector.connect()
     except:
