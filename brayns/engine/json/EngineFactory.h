23acd9fbc92e1035e68e866e6ff997d52479708d
Nadir Roman Guerrero <nadir.ro.gue@gmail.com>
2023-01-18 11:39:20 +0100

    Remove schemas

--- brayns/engine/json/EngineFactory.h
+++ brayns/engine/json/EngineFactory.h
@@ -47,7 +47,6 @@ public:
         virtual ~IFactoryEntry() = default;
         virtual T deserialize(const JsonValue &payload) const = 0;
         virtual JsonValue serialize(const T &object) const = 0;
-        virtual JsonSchema getSchema() const = 0;
         virtual std::string_view getName() const noexcept = 0;
     };
 
@@ -82,13 +81,6 @@ public:
             }
 
             return Json::serialize(casted);
-        }
-
-        JsonSchema getSchema() const override
-        {
-            auto schema = Json::getSchema<SubT>();
-            schema.title = _name;
-            return schema;
         }
 
         std::string_view getName() const noexcept override
@@ -128,16 +120,9 @@ public:
     {
         using Traits = typename T::template Traits<SubT>;
         _items.push_back(std::make_unique<FactoryEntry<SubT>>(Traits::name));
-        _schemas.push_back(_items.back()->getSchema());
-    }
-
-    const std::vector<JsonSchema> &getSchemas() const noexcept
-    {
-        return _schemas;
     }
 
 private:
     std::vector<std::unique_ptr<IFactoryEntry>> _items;
-    std::vector<JsonSchema> _schemas;
 };
 }
