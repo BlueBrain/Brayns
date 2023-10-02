be23af5dd126092e3afda71c2f78eb3e809be524
Nadir Roman Guerrero <nadir.ro.gue@gmail.com>
2023-10-02 12:03:39 +0200

    remove unused import

--- python/testapi/plugins/test_morphology.py
+++ python/testapi/plugins/test_morphology.py
@@ -19,7 +19,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import brayns
-from testapi.render import render_and_validate, render_and_save
+from testapi.render import render_and_validate
 from testapi.simple_test_case import SimpleTestCase
 
 
